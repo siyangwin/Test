4d5d2d8c1e5e6bb0287d8ebd2761834fa0a5cc78
SiYang <59567021+siyangwin@users.noreply.github.com>
2024-06-18 15:05:40 +0800

    MMMS[Progress Sheet]订单号生成测试，修改

--- Program.cs
+++ Program.cs
@@ -10,19 +10,31 @@ namespace Test
             //Console.WriteLine("Hello, World!");
 
             #region MMMS[Progress Sheet]订单号生成测试
-            //订单号模板
-            string initialNumber = "PS00000";
+            //订单号模板，第一个订单按这个来
+            string initialNumber = "PS2024061800000";
 
             //获取数据库中最大的订单号
+            //兼容旧订单号码
             string maxCaseNo = "PS00001";
+            //新订单
+            maxCaseNo = "PS2024061800009";
 
             if (maxCaseNo != null)
             {
                 initialNumber = maxCaseNo;
             }
 
-            //去掉前两位
-            string numberPart = initialNumber[2..];
+            string numberPart = "";
+            if (initialNumber.Length == 7)
+            {
+                //去掉前两位
+                numberPart = initialNumber[2..];
+            }
+            else
+            {
+                //去掉前十位
+                numberPart = initialNumber[10..];
+            }
 
             //转换为int类型
             int number = int.Parse(numberPart);
@@ -30,7 +42,7 @@ namespace Test
             number++;
 
             //补齐前面的空缺0,位数补齐
-            string incrementedNumber = string.Concat(initialNumber.AsSpan(0, 2), number.ToString("D5"));
+            string incrementedNumber = string.Concat(initialNumber.AsSpan(0, 2),DateTime.Now.ToString("yyyyMMdd"), number.ToString("D5"));
 
             //得到最新的订单号
             string caseNo = incrementedNumber;
