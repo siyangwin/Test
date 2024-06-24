6076474cf5a25af5cf2ffd9e264cf4ab4fa4dc43
RisingUp <RisingUp>
2024-06-24 16:30:59 +0800

    更新  MMMS[Progress Sheet]枚举测试

--- Program.cs
+++ Program.cs
@@ -9,6 +9,23 @@ namespace Test
             // See https://aka.ms/new-console-template for more information
             //Console.WriteLine("Hello, World!");
 
+            //MMMS[Progress Sheet]订单号生成测试
+            //MMMSCreateOrderNo();
+
+
+            //MMMS[Progress Sheet]枚举测试
+            MMMSEnum();
+
+
+            //Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// MMMS[Progress Sheet]订单号生成测试
+        /// </summary>
+        public static void MMMSCreateOrderNo()
+        {
             #region MMMS[Progress Sheet]订单号生成测试
             //订单号模板，第一个订单按这个来
             string initialNumber = "PS2024061800000";
@@ -42,7 +59,7 @@ namespace Test
             number++;
 
             //补齐前面的空缺0,位数补齐
-            string incrementedNumber = string.Concat(initialNumber.AsSpan(0, 2),DateTime.Now.ToString("yyyyMMdd"), number.ToString("D5"));
+            string incrementedNumber = string.Concat(initialNumber.AsSpan(0, 2), DateTime.Now.ToString("yyyyMMdd"), number.ToString("D5"));
 
             //得到最新的订单号
             string caseNo = incrementedNumber;
@@ -51,8 +68,38 @@ namespace Test
             Console.WriteLine(caseNo);
             Console.WriteLine(numberPart);
             #endregion
+        }
 
-            //Console.ReadKey();
+        /// <summary>
+        /// MMMS[Progress Sheet]枚举测试
+        /// </summary>
+        public static void MMMSEnum()
+        {
+            ProgressSheetStatusEnum progressSheetStatusEnum= new ProgressSheetStatusEnum();
+            //按枚举获取string值
+            //根據枚舉成員獲取自定義屬性EnumDisplayNameAttribute的屬性DisplayName
+            string EnumValue = EnumExtension.GetEnumCustomDescription(progressSheetStatusEnum);
+             Console.WriteLine(EnumValue);
+
+            //獲取枚舉項描述信息 
+            string EnumDesc = EnumExtension.GetEnumDesc(ProgressSheetStatusEnum.Completed);
+            Console.WriteLine(EnumValue);
+
+
+            //獲取枚舉項描述信息
+            EnumDesc = EnumExtension.GetEnumDesc(ProgressSheetStatusEnum.Completed,"");
+            Console.WriteLine(EnumValue);
+
+            //獲取枚舉的描述文本
+            string EnumDescription = EnumExtension.GetDescription(ProgressSheetStatusEnum.Completed);
+            Console.WriteLine(EnumDescription);
+
+
+            EnumDescription = EnumExtension.GetDescription(typeof(ProgressSheetStatusEnum), 10);
+            Console.WriteLine(EnumDescription);
+
+            string description = EnumExtension.GetEnumDesc(typeof(ProgressSheetStatusEnum), 10);
+            Console.WriteLine(description);  // 输出: Second Value
         }
     }
 }
---
+++ ProgressSheetEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public enum ProgressSheetEnum
+    {
+    }
+
+    public enum ProgressSheetStatusEnum
+    {
+        /// <summary>
+        /// 草稿
+        /// </summary>
+        Draft = 10,
+
+        /// <summary>
+        /// 进行中
+        /// </summary>
+        WIPPharmacy = 20,
+
+        /// <summary>
+        /// 已完成
+        /// </summary>
+        Completed = 30
+    }
+}
